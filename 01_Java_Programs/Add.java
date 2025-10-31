e0821fbe6f3bde490cff689d446190b416616fbf
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-10-31 08:09:19 +0530

    Add files via upload

---
+++ 01_Java_Programs/Add.java
@@ -0,0 +1,8 @@
+class Add {
+	public static void main(String args[]) {
+		int num1 = 56, num2 = 44; // Variable declaration
+		int result;
+		result = num1 + num2;    // Expression
+		System.out.println("Addition is: " + result);
+	}
+}
