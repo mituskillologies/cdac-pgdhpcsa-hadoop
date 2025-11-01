9cd7c304d48103a257499b81f20c36da23aa6f62
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-11-01 11:54:42 +0530

    Add files via upload

---
+++ 01_Java_Programs/ArrayOperations.java
@@ -0,0 +1,9 @@
+class ArrayOperations{
+	public static void main(String args[]) {
+		int num[] = {56,12,32,54,86,24,38};
+		
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
+		}	
+	}
+}
