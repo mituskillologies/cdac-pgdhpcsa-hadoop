fffe6ffde1aac96b28973910002b076fb6a03e1b
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-11-03 08:24:21 +0530

    Add files via upload

---
+++ 01_Java_Programs/Strings.java
@@ -0,0 +1,10 @@
+class StringOperations {
+	public static void main(String args[]) {
+		int num[] = {56,11,37,54,86,25,38};
+		
+		for (int n : num) {
+			if (n % 2 == 1)
+				System.out.println(n);
+		}
+	}
+}
