7e99c7e8f24dae73fec68efc2af2a333bdde8d3a
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-11-05 08:28:48 +0530

    Add files via upload

---
+++ 01_Java_Programs/Function1.java
@@ -0,0 +1,15 @@
+class Function1 {
+	int larger(int n1, int n2) {
+		if (n1 > n2) 
+			return n1;
+		else
+			return n2;
+	}
+	public static void main(String args[]) {
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		Function1 f = new Function1();  // create object
+		int large = f.larger(num1, num2); // method call
+		System.out.println("Larger:"+ large);
+	}
+}
