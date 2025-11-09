f6ff74700e6ea282f4e1dd74812314786e274866
MITU Skillologies <120009508+mituskillologies@users.noreply.github.com>
2025-11-09 13:17:01 +0530

    Add files via upload

---
+++ 01_Java_Programs/Overriding.java
@@ -0,0 +1,29 @@
+class Flower {					// class declaration
+	String name;
+	int num_petals;
+	Flower(String name, int num_petals) {
+		this.name = name;
+		this.num_petals = num_petals;
+	}
+	void show() {
+		System.out.println("Flower: " + this.name);
+		System.out.println("Petals: " + this.num_petals);
+	}
+}
+class Rose extends Flower {
+	String color;
+	Rose(String n, int p, String c) {
+		super(n, p);
+		color = c;
+	}
+	void show() {
+		super.show();
+		System.out.println("Color: " + color);
+	}
+}
+class Overriding {
+	public static void main(String args[]) {
+		Rose r = new Rose("Red Rose",10,"Maroon");
+		r.show();
+	}
+}
